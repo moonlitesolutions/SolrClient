43c7fa3a4f6f57baadcb5ddaef093d9d626d1c2d
Nick Vasilyev <wrdrvr@gmail.com>
2015-10-14 22:35:56 -0400

    fixed testing glitch

--- test/RandomTestData.py
+++ test/RandomTestData.py
@@ -15,9 +15,13 @@ class RandomTestData:
         self.logger = logging.getLogger(__package__)
         self.fields = test_config['collections']['fields']
         self.fields.append({'name':'id','type':'id_string'})
-        for field in self.fields:
+        to_delete = []
+        for i, field in enumerate(self.fields):
             if field['name'] == 'facet_test':
                 field['type'] = 'facet_string'
+            elif field['name'].endswith('exact'):
+                to_delete.append(i)
+        [self.fields.pop(i) for i in to_delete]
         self.lorem = '''
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac elementum lacus. Integer eu nulla vel nisi pretium posuere quis sit amet nulla. Nunc ornare consectetur quam a tristique. Vivamus vel vestibulum enim. Praesent mattis, lacus sit amet eleifend ultricies, ipsum nisi commodo elit, sed varius diam quam at dui. Praesent non aliquet nisi. Vestibulum bibendum auctor ligula. Vestibulum hendrerit quam vitae risus ullamcorper, in dapibus arcu fringilla. Vestibulum auctor tincidunt orci, at elementum libero tempus vel. Morbi imperdiet odio nunc, sit amet venenatis sem accumsan in. Mauris vulputate neque a ipsum fermentum mattis. In fringilla consectetur orci. Ut rutrum, enim accumsan tincidunt congue, diam dolor fermentum sapien, ac mattis nulla mauris non nisl. In auctor consectetur porttitor. Vivamus vel suscipit purus. Nunc lacinia ex vitae semper feugiat.
         '''
