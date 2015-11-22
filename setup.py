a551b8fff5ea23706c6e7e73a386b6ef68b7b5fa
Nick Vasilyev <nickvasilyev@users.noreply.github.com>
2015-11-22 18:24:41 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "SolrClient",
-    version = "0.0.5",
+    version = "0.0.6",
     author = "Nick Vasilyev",
     author_email = "nick.vasilyev1@gmail.com",
     packages = find_packages(),
