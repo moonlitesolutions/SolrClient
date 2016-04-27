2479d8dedc24b695dabfff64499d6da11cb7e902
Nick Vasilyev <wrdrvr@gmail.com>
2016-04-27 08:34:48 -0400

    Collection Validation. Experimental

--- SolrClient/collections.py
+++ SolrClient/collections.py
@@ -4,6 +4,7 @@ from .transport import TransportRequests
 from .schema import Schema
 from .exceptions import *
 from .solrresp import SolrResponse
+from collections import defaultdict
 
 class Collections():
     '''
@@ -28,3 +29,91 @@ class Collections():
             return res, con_info
         else:
             raise SolrError("Error Issuing Collections API Call for: {} +".format(con_info, res))
+    
+    def clusterstatus(self):
+        '''
+        Returns a slightly slimmed down version of the clusterstatus api command. 
+        '''
+        res, con_info =  self.api('clusterstatus')
+        cluster = res['cluster']['collections']
+        out = {}
+        try:
+            for collection in cluster:
+                out[collection] = {}
+                for shard in cluster[collection]['shards']:
+                    out[collection][shard] = {}
+                    for replica in cluster[collection]['shards'][shard]['replicas']:
+                        out[collection][shard][replica] = cluster[collection]['shards'][shard]['replicas'][replica]
+                        if out[collection][shard][replica]['state'] != 'active':
+                            self.logger.error("{}->{}->{} is not Active".format(collection, shard, replica))
+        except Exception as e:
+            self.logger.error("Couldn't parse response from clusterstatus API call")
+            self.logger.exception(e)
+        return out
+    
+
+    def _for_core(self, cluster_resp=None):
+        if cluster_resp is None:
+            cluster_resp = self.clusterstatus()
+        for collection in cluster_resp:
+            for shard in cluster_resp[collection]:
+                for core in cluster_resp[collection][shard]:
+                    yield collection, shard, core, cluster_resp[collection][shard][core]
+
+    def _for_shard(self, cluster_resp=None):
+        if cluster_resp is None:
+            cluster_resp = self.clusterstatus()
+        for collection in cluster_resp:
+            for shard in cluster_resp[collection]:
+                yield collection, shard, cluster_resp[collection][shard]
+                    
+    def _check_collection(self, collection, cluster_resp=None ):
+        for coll, shard, core, c_data in self._for_core(cluster_resp):
+            if collection == coll:
+                if c_data['state'] != 'active':
+                    return False
+                return True
+        self.logger.error("Couldn't find Collection {} in cluster_resp".format(collection))
+        
+    def get_collection_counts(self, cluster_resp=None):
+        '''
+        Queries each core to get individual counts for each core for each shard. 
+        '''
+        from SolrClient import SolrClient
+        temp = {}
+        for coll, shard, core, c_data in self._for_core(cluster_resp):
+            if coll not in temp:
+                temp[coll] = {}
+            if shard not in temp[coll]:
+                temp[coll][shard] = {}
+            ts = SolrClient( c_data['base_url'] )
+            temp[coll][shard][c_data['core']] = ts.query(c_data['core'], 
+                                                        {'q':'*:*',
+                                                        'rows':0,
+                                                        'distrib': 'false',
+                                                        }).get_num_found()
+        return temp
+        
+    def check_collection_counts(self, counts=None, cb=None):
+        if counts is None:
+            counts = self.get_collection_counts()
+        for collection, shard, s_data in self._for_shard(counts):
+            for replica in s_data:
+                self.logger.info("Item Count for {}->{}->{}: {}".format(collection, shard, replica, s_data[replica]))
+            if len(set(s_data.values())) > 1:
+                self.logger.error("Count Mismatch for {}->{}->{} {}".format( collection, shard, replica, "/".join([str(x) for x in list(s_data.values())])))
+                if cb:
+                    if hasattr(cb, '__call__'):
+                        self.logger.debug("Calling callback on collection count mismatch")
+                        cb(s_data)
+                    else:
+                        raise TypeError("Callback passed to check_collection_counts is not callable")
+        return counts
+        
+    def check_collections(self, collection=None):
+        cluster = self.clusterstatus()
+        out = {}
+        for coll in cluster.keys():
+            out[coll] = self._check_collection(coll, cluster)
+            self.logger.info("Collection {} is {}".format(coll, "Active" if out[coll] else "having issues"))
+        return out
--- test/test_collections.py
+++ test/test_collections.py
@@ -8,8 +8,10 @@ from SolrClient import SolrClient
 from SolrClient.exceptions import *
 from .test_config import test_config
 from .RandomTestData import RandomTestData
+from unittest.mock import Mock
 import random
-#logging.basicConfig(level=logging.DEBUG,format='%(asctime)s [%(levelname)s] (%(process)d) (%(threadName)-10s) [%(name)s] %(message)s')
+
+logging.basicConfig(level=logging.DEBUG,format='%(asctime)s [%(levelname)s] (%(process)d) (%(threadName)-10s) [%(name)s] %(message)s')
 
 class test_collections(unittest.TestCase):
     #High Level Client Tests
@@ -17,7 +19,7 @@ class test_collections(unittest.TestCase):
         self.solr = SolrClient(test_config['SOLR_SERVER'][0], devel=True)
   
     def test_create_collection_no_args(self):
-        with self.assertRaises(ValueError):
+        with self.assertRaises(SolrError):
             self.solr.collections.api('action', {})
             
     def test_create_collection(self):
@@ -42,6 +44,36 @@ class test_collections(unittest.TestCase):
         #Clean up and delete the collection
         res, con_info = self.solr.collections.api('delete', {'name':temp})
         self.assertTrue('success' in res)
+    
+    def test_get_clusterstatus(self):
+        c = self.solr.collections.clusterstatus()
+        self.assertTrue(type(c) is dict)
+        self.assertTrue(len(c.keys()) > 1)
         
+    def test_get_collection_counts(self):
+        c = self.solr.collections.get_collection_counts()
+        self.assertTrue(type(c) is dict)
+        self.assertTrue(len(c.keys()) > 1)
+
+                
+    def test_check_collection_counts_callback(self):
+        cb = Mock()
+        self.solr.collections.check_collection_counts(
+                {'banana_int16': {'shard1': {'banana_int16_shard1_replica1': 2,
+                                                'banana_int16_shard1_replica2': 0}}},
+                cb
+                )
+        cb.assert_called_with({'banana_int16_shard1_replica2': 0, 'banana_int16_shard1_replica1': 2})
+    
+    def test_check_collection_counts_bad_callback(self):
+        a = 'NOT a callable'
+        with self.assertRaises(TypeError):
+            self.solr.collections.check_collection_counts(
+                {'banana_int16': {'shard1': {'banana_int16_shard1_replica1': 2,
+                                                'banana_int16_shard1_replica2': 0}}},
+                a
+                )
+        
+    
 if __name__=='__main__':
     pass
