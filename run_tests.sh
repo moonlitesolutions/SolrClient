5d7e595f579d1e68e48fec3eb30a258b6df48863
nick <wrdrvr@gmail.com>
2016-05-13 11:09:26 -0400

    better testing rig

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+export SOLR_COLLECTION=SolrClient_unittest
 
-python3 -m unittest test.test_client
-python3 -m unittest test.test_resp
-python3 -m unittest test.test_indexq
-python3 -m unittest test.test_collections
-python3 -m unittest test.test_reindexer
+for version in "5.2.1" "5.3.1" "6.0.0"
+do
+  export SOLR_TEST_URL=http://localhost:9`echo $version | sed -r 's/\.//g'`/solr
+  python3 -m unittest test.test_client
+  python3 -m unittest test.test_resp
+  python3 -m unittest test.test_indexq
+  python3 -m unittest test.test_collections
+  python3 -m unittest test.test_reindexer
+done
