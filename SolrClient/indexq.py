297eab5ffaa5346fc3d10325f4cf76caf7ae8623
nick <wrdrvr@gmail.com>
2016-09-23 23:01:31 -0400

    - Added rotate_complete call into indexq for rotation of complete files - Added compress_complete parameter to compress done files

--- SolrClient/indexq.py
+++ SolrClient/indexq.py
@@ -16,72 +16,75 @@ from SolrClient.exceptions import *
 
 class IndexQ():
     '''
-    IndexQ sub module will help with indexing content into Solr. It can be used to de-couple data processing from indexing. 
-    
+    IndexQ sub module will help with indexing content into Solr. It can be used to de-couple data processing from indexing.
+
     Each queue is set up with the following directory structure
-    queue_name/ 
+    queue_name/
      - todo/
-     - done/ 
-    
-    Items get saved to the todo directory and once an item is processed it gets moved to the done directory. Items are processed in chronological order. 
+     - done/
+
+    Items get saved to the todo directory and once an item is processed it gets moved to the done directory. Items are processed in chronological order.
     '''
 
-    def __init__(self, basepath, queue, compress=False, size=0, devel=False, threshold = 0.90, log = None, **kwargs ):
+    def __init__(self, basepath, queue, compress=False, compress_complete=False, size=0, devel=False,
+                 threshold=0.90, log=None, rotate_complete=None, **kwargs ):
         '''
-        :param string basepath: Path to the root of the indexQ. All other queues will get created underneath this. 
-        :param string queue: Name of the queue. 
-        :param log: Logging instance that you want it to log to. 
+        :param string basepath: Path to the root of the indexQ. All other queues will get created underneath this.
+        :param string queue: Name of the queue.
+        :param log: Logging instance that you want it to log to.
+        :param rotate_complete: Supply a callable that will be used to store completed files. Completed files will be moved to /done/`callable_output`/.
         :param bool compress: If todo files should be compressed, set to True if there is going to be a lot of data and these files will be sitting there for a while.
-        :param int size: Internal buffer size (MB) that queued data must be to get written to the file system. If not passed, the data will be written to the filesystem as it is sent to IndexQ, otherwise they will be written when the buffer reaches 90%. 
-        
+        :param bool compress: If done files should be compressed, set to True if there is going to be a lot of data and these files will be sitting there for a while.
+        :param int size: Internal buffer size (MB) that queued data must be to get written to the file system. If not passed, the data will be written to the filesystem as it is sent to IndexQ, otherwise they will be written when the buffer reaches 90%.
+
         Example Usage::
             i = IndexQ('/data/indexq','parsed_data')
-            
+
         '''
         self.logger = log or logging.getLogger(__package__)
-        
+
         self._basepath = basepath
         self._queue_name = queue
         self._compress = compress
+        self._compress_complete = compress_complete
         self._size = size
         self._devel = devel
         self._threshold = threshold
-        self._qpathdir = os.path.join(self._basepath,self._queue_name)
-        self._todo_dir = os.path.join(self._basepath,self._queue_name,'todo')
-        self._done_dir = os.path.join(self._basepath,self._queue_name,'done')
+        self._qpathdir = os.path.join(self._basepath, self._queue_name)
+        self._todo_dir = os.path.join(self._basepath, self._queue_name, 'todo')
+        self._done_dir = os.path.join(self._basepath, self._queue_name, 'done')
         self._locked = False
         self._rlock = threading.RLock()
-        
-        #Lock File        
+        self.rotate_complete = rotate_complete
+        #Lock File
         self._lck = os.path.join(self._qpathdir,'index.lock')
-        
+
         for dir in [self._qpathdir, self._todo_dir, self._done_dir]:
             if not os.path.isdir(dir):
                 os.makedirs(dir)
-        
+
         #First argument will be datestamp, second is counter
         self._output_filename_pattern = self._queue_name+"_{}.json"
         self._preprocess = self._buffer(self._size*1000000, self._write_file)
-        
-            
         self.logger.info("Opening Queue {}".format(queue))
-    
-    #This part is all about loading data
+
+
     def _gen_file_name(self):
         '''
-        Generates a random file name based on self._output_filename_pattern for the output to do file. 
+        Generates a random file name based on self._output_filename_pattern for the output to do file.
         '''
         date = datetime.datetime.now()
         dt = "{}-{}-{}-{}-{}-{}-{}".format(str(date.year),str(date.month),str(date.day),str(date.hour),str(date.minute),str(date.second),str(random.randint(0,10000)))
-        return self._output_filename_pattern.format(dt) 
-            
+        return self._output_filename_pattern.format(dt)
+
+
     def add(self, item=None, finalize=False, callback=None):
         '''
-        Takes a string, dictionary or list of items for adding to queue. To help troubleshoot it will output the updated buffer size, however when the content gets written it will output the file path of the new file. Generally this can be safely discarded. 
-        
-        :param <dict,list> item: Item to add to the queue. If dict will be converted directly to a list and then to json. List must be a list of dictionaries. If a string is submitted, it will be written out as-is immediately and not buffered. 
+        Takes a string, dictionary or list of items for adding to queue. To help troubleshoot it will output the updated buffer size, however when the content gets written it will output the file path of the new file. Generally this can be safely discarded.
+
+        :param <dict,list> item: Item to add to the queue. If dict will be converted directly to a list and then to json. List must be a list of dictionaries. If a string is submitted, it will be written out as-is immediately and not buffered.
         :param bool finalize: If items are buffered internally, it will flush them to disk and return the file name.
-        :param callback: A callback function that will be called when the item gets written to disk. It will be passed one position argument, the file path of the file written. Note that errors from the callback method will not be re-raised here. 
+        :param callback: A callback function that will be called when the item gets written to disk. It will be passed one position argument, the file path of the file written. Note that errors from the callback method will not be re-raised here.
         '''
         if item:
             if type(item) is list:
@@ -97,6 +100,7 @@ class IndexQ():
         with self._rlock:
             res = self._preprocess(item, finalize, callback)
         return res
+
 
     def _write_file(self, content):
         while True:
@@ -114,7 +118,7 @@ class IndexQ():
                 f.write(content)
         return path
 
-        
+
     def _buffer(self, size, callback):
         _c = {
             'size': 0,
@@ -124,7 +128,7 @@ class IndexQ():
         }
         self.logger.debug("Starting Buffering Queue with Size of {}".format(size))
         def inner(item=None, finalize=False, listener=None):
-            #Listener is the external callback specific by the user. Need to change the names later a bit. 
+            #Listener is the external callback specific by the user. Need to change the names later a bit.
             if item:
                 #Buffer Item
                 [_c['buf'].append(x) for x in item]
@@ -155,6 +159,7 @@ class IndexQ():
             return _c['size']
         return inner
 
+
     #This is about pullind data out
     def _lock(self):
         '''
@@ -167,7 +172,8 @@ class IndexQ():
             return True
         else:
             return False
-    
+
+
     def _is_locked(self):
         '''
         Checks to see if we are already pulling items from the queue
@@ -183,7 +189,8 @@ class IndexQ():
             return True if psutil.pid_exists(int(pid)) else False
         else:
             return False
-        
+
+
     def _unlock(self):
         '''
         Unlocks the index
@@ -194,86 +201,125 @@ class IndexQ():
             return True
         else:
             return True
-        
-    def get_all_as_list(self,dir='_todo_dir'):
+
+
+    def get_all_as_list(self, dir='_todo_dir'):
         '''
-        Returns a list of the the full path to all items currently in the todo directory. The items will be listed in ascending order based on filesystem time. 
-        This will re-scan the directory on each execution. 
-        
-        Do not use this to process items, this method should only be used for troubleshooting or something axillary. To process items use get_todo_items() iterator. 
+        Returns a list of the the full path to all items currently in the todo directory. The items will be listed in ascending order based on filesystem time.
+        This will re-scan the directory on each execution.
+
+        Do not use this to process items, this method should only be used for troubleshooting or something axillary. To process items use get_todo_items() iterator.
         '''
         dir = getattr(self,dir)
         list = [x for x in os.listdir(dir) if x.endswith('.json') or x.endswith('.json.gz')]
         full = [os.path.join(dir,x) for x in list]
         full.sort(key=lambda x: os.path.getmtime(x))
         return full
-        
 
-       
-    def get_todo_items(self,**kwargs):
+
+    def get_todo_items(self, **kwargs):
         '''
-        Returns an iterator that will provide each item in the todo queue. Note that to complete each item you have to run complete method with the output of this iterator. 
-        
-        That will move the item to the done directory and prevent it from being retrieved in the future. 
+        Returns an iterator that will provide each item in the todo queue. Note that to complete each item you have to run complete method with the output of this iterator.
+
+        That will move the item to the done directory and prevent it from being retrieved in the future.
         '''
         def inner(self):
             for item in self.get_all_as_list():
                 yield item
             self._unlock()
-            
+
         if not self._is_locked():
             if self._lock():
                 return inner(self)
         raise RuntimeError("RuntimeError: Index Already Locked")
-    
+
+
     def complete(self, filepath):
         '''
-        Marks the item as complete by moving it to the done directory and optionally gzipping it. 
+        Marks the item as complete by moving it to the done directory and optionally gzipping it.
         '''
-        
+
         #TODO: Implement compress option to also gzip these completed items
         if not os.path.exists(filepath):
             raise FileNotFoundError("Can't Complete {}, it doesn't exist".format(filepath))
         if self._devel: self.logger.debug("Completing - {} ".format(filepath))
-        newpath = os.path.join(self._done_dir,os.path.split(filepath)[-1])
+        if self.rotate_complete:
+            try:
+                complete_dir = str(self.rotate_complete())
+            except Exception as e:
+                self.logger.error("rotate_complete function failed with the following exception.")
+                self.logger.exception(e)
+                raise
+            newpath = os.path.join(self._done_dir, complete_dir, os.path.split(filepath)[-1] )
+            if not os.path.isdir(newpath):
+                os.makedirs(newpath)
+        else:
+            newpath = os.path.join(self._done_dir, os.path.split(filepath)[-1] )
         try:
+            if self._compress_complete:
+                if not filepath.endswith('.gz'):
+                    #  Compressing complete, but existing file not compressed
+                    #  Compress and move it and kick out
+                    newpath += '.gz'
+                    self._compress_and_move(filepath, newpath)
+                    return newpath
+            if filepath.endswith('.gz') and self._compress_complete == True:
+                raise RuntimeError("You don't need to enable compression of \
+                                    complete files if the incoming todo files \
+                                    are already compressed. ")
             shutil.move(filepath, newpath)
+
             self.logger.info(" Completed - {}".format(filepath))
-        except:
+        except Exception as e:
             self.logger.error("Couldn't Complete {}".format(filepath))
+            self.logger.exception(e)
             raise
-                
+        return newpath
+
+
+    def _compress_and_move(self, source, destination):
+        try:
+            with gzip.open(destination, 'wb') as df, open(source, 'rb') as sf:
+                    df.writelines(sf)
+            os.remove(source)
+        except Exception as e:
+            self.logger.error("Unable to Compress and Move file {} -> {}".format(source, destination))
+            self.logger.exception(e)
+            raise
+        return True
+
 
     def index(self, solr, collection, threads=1, send_method='stream_file', **kwargs):
         '''
-        Will index the queue into a specified solr instance and collection. Specify multiple threads to make this faster, however keep in mind that if you specify multiple threads the items may not be in order.  
+        Will index the queue into a specified solr instance and collection. Specify multiple threads to make this faster, however keep in mind that if you specify multiple threads the items may not be in order.
         Example::
             solr = SolrClient('http://localhost:8983/solr/')
             for doc in self.docs:
                 index.add(doc, finalize=True)
             index.index(solr,'SolrClient_unittest')
-            
-        :param object solr: SolrClient object
-        :param string collection: The name of the collection to index document into. 
-        :param int threads: Number of simultaneous threads to spin up for indexing. 
+
+        :param object solr: SolrClient object.
+        :param string collection: The name of the collection to index document into.
+        :param int threads: Number of simultaneous threads to spin up for indexing.
         :param string send_method: SolrClient method to execute for indexing. Default is stream_file
         '''
+
         try:
             method = getattr(solr, send_method)
         except AttributeError:
             raise AttributeError("Couldn't find the send_method. Specify either stream_file or local_index")
-        
+
         self.logger.info("Indexing {} into {} using {}".format(self._queue_name, collection, send_method))
         def _wrap(method, collection, index, doc):
             try:
-                res = method(collection,doc)
+                res = method(collection, doc)
                 if res:
                     index.complete(doc)
                 return res
             except SolrError:
                 self.logger.error("Error Indexing Item: {}".format(doc))
                 pass
-                
+
         if threads > 1:
             method = partial(_wrap, method, collection, self)
             with ThreadPool(threads) as p:
@@ -289,9 +335,10 @@ class IndexQ():
                     self._unlock()
                     raise
 
+
     def get_all_json_from_indexq(self):
         '''
-        Gets all data from the todo files in indexq and returns one huge list of all data. 
+        Gets all data from the todo files in indexq and returns one huge list of all data.
         '''
         files = self.get_all_as_list()
         out = []
@@ -305,17 +352,18 @@ class IndexQ():
             out.extend(f_data)
         return out
 
+
     def get_multi_q(self, sentinel='STOP'):
         '''
-        This helps indexq operate in multiprocessing environment without each process having to have it's own IndexQ. It also is a handy way to deal with thread / process safety. 
+        This helps indexq operate in multiprocessing environment without each process having to have it's own IndexQ. It also is a handy way to deal with thread / process safety.
 
-        This method will create and return a JoinableQueue object. Additionally, it will kick off a back end process that will monitor the queue, de-queue items and add them to this indexq. 
+        This method will create and return a JoinableQueue object. Additionally, it will kick off a back end process that will monitor the queue, de-queue items and add them to this indexq.
 
-        The returned JoinableQueue object can be safely passed to multiple worker processes to populate it with data. 
+        The returned JoinableQueue object can be safely passed to multiple worker processes to populate it with data.
 
         To indicate that you are done writing the data to the queue, pass in the sentinel value ('STOP' by default).
 
-        Make sure you call join_indexer() after you are done to close out the queue and join the worker. 
+        Make sure you call join_indexer() after you are done to close out the queue and join the worker.
         '''
         self.in_q = JoinableQueue()
         self.indexer_process = Process(target=self._indexer_process, args=(self.in_q, sentinel))
@@ -323,11 +371,13 @@ class IndexQ():
         self.indexer_process.start()
         return self.in_q
 
+
     def join_indexer(self):
         self.logger.info("Joining Queue")
         self.in_q.join()
         self.logger.info("Joining Index Process")
         self.indexer_process.join()
+
 
     def _indexer_process(self, in_q, sentinel):
         self.logger.info("Indexing Process Started")
@@ -337,7 +387,7 @@ class IndexQ():
         seen_STOP = False
         while True:
             if seen_STOP and in_q.qsize() == 0:
-                #If sentinel has been seen and the queue size is zero, write out the queue and return. 
+                #If sentinel has been seen and the queue size is zero, write out the queue and return.
                 self.logger.info("Indexer Queue is empty. Stopping....")
                 self.add(finalize=True)
                 return
@@ -353,10 +403,8 @@ class IndexQ():
                 seen_STOP = True
                 in_q.task_done()
                 continue
-
             count += 1
             total += 1
-
             self.add(item)
             in_q.task_done()
 
@@ -364,5 +412,3 @@ class IndexQ():
                 self.logger.debug("Indexed {} items in the last 60 seconds. Total: ".format(count, total))
                 count = 0
                 stime = time.time()
-
-
