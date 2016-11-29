c3b206c5df235d3e534197a7f5c766c018bd0519
Dorian <dorian.hoxha@gmail.com>
2016-11-29 13:51:20 +0100

    pep8 test_schema.py

--- test/test_schema.py
+++ test/test_schema.py
@@ -3,16 +3,17 @@ import logging
 from SolrClient import SolrClient
 from .test_config import test_config
 
-
-#logging.basicConfig(level=logging.DEBUG,format='%(asctime)s [%(levelname)s] (%(process)d) (%(threadName)-10s) [%(name)s] %(message)s')
+# logging.basicConfig(level=logging.DEBUG,format='%(asctime)s [%(levelname)s] (%(process)d) (%(threadName)-10s) [%(name)s] %(message)s')
 logging.disable(logging.CRITICAL)
+
+
 class schemaTest(unittest.TestCase):
-    #High Level Client Tests
+    # High Level Client Tests
     def setUp(self):
-        self.solr = SolrClient(test_config['SOLR_SERVER'][0],devel=True,auth=test_config['SOLR_CREDENTIALS'])
+        self.solr = SolrClient(test_config['SOLR_SERVER'][0], devel=True, auth=test_config['SOLR_CREDENTIALS'])
 
     def delete_fields(self):
-        #Fix this later to check for field before sending a delete
+        # Fix this later to check for field before sending a delete
         for copy_field in self.solr.schema.get_schema_copyfields(test_config['SOLR_COLLECTION']):
             try:
                 self.solr.schema.delete_copy_field(test_config['SOLR_COLLECTION'], copy_field)
@@ -20,43 +21,41 @@ class schemaTest(unittest.TestCase):
                 logging.info("tried to delete copy field that didn't exist")
                 pass
         for field in test_config['collections']['fields']:
-            if self.solr.schema.does_field_exist(test_config['SOLR_COLLECTION'],field['name']):
-                self.solr.schema.delete_field(test_config['SOLR_COLLECTION'],field['name'])
-
-
+            if self.solr.schema.does_field_exist(test_config['SOLR_COLLECTION'], field['name']):
+                self.solr.schema.delete_field(test_config['SOLR_COLLECTION'], field['name'])
 
     def create_fields(self):
         for field in test_config['collections']['fields']:
-            self.solr.schema.create_field(test_config['SOLR_COLLECTION'],field)
+            self.solr.schema.create_field(test_config['SOLR_COLLECTION'], field)
 
     def create_copy_fields(self):
         for field in test_config['collections']['copy_fields']:
-            self.solr.schema.create_copy_field(test_config['SOLR_COLLECTION'],field)
+            self.solr.schema.create_copy_field(test_config['SOLR_COLLECTION'], field)
 
-    def check_fields(self,result):
+    def check_fields(self, result):
         if result is True:
             for field in test_config['collections']['fields']:
-                self.assertTrue(self.solr.schema.does_field_exist(test_config['SOLR_COLLECTION'],field['name']))
+                self.assertTrue(self.solr.schema.does_field_exist(test_config['SOLR_COLLECTION'], field['name']))
         elif result is False:
             for field in test_config['collections']['fields']:
-                self.assertFalse(self.solr.schema.does_field_exist(test_config['SOLR_COLLECTION'],field['name']))
+                self.assertFalse(self.solr.schema.does_field_exist(test_config['SOLR_COLLECTION'], field['name']))
 
     def test_create_fields(self):
-        #Delete Fields if they exist
+        # Delete Fields if they exist
         self.delete_fields()
-        #Check to Make Sure fields were deleted
+        # Check to Make Sure fields were deleted
         self.check_fields(False)
-        #Create new Fields
+        # Create new Fields
         self.create_fields()
-        #Make sure they are there
+        # Make sure they are there
         self.check_fields(True)
-        #Create Copy Fields
+        # Create Copy Fields
         self.create_copy_fields()
 
     def test_check_if_field_exists(self):
-        self.assertTrue(self.solr.schema.does_field_exist(test_config['SOLR_COLLECTION'],'id'))
-        self.assertFalse(self.solr.schema.does_field_exist(test_config['SOLR_COLLECTION'],'asdf432qftr43fsfgreg'))
+        self.assertTrue(self.solr.schema.does_field_exist(test_config['SOLR_COLLECTION'], 'id'))
+        self.assertFalse(self.solr.schema.does_field_exist(test_config['SOLR_COLLECTION'], 'asdf432qftr43fsfgreg'))
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     pass
