b76a5e0f35f97141ee68c34331c2e8eee53fe4d4
Dorian <dorian.hoxha@gmail.com>
2016-11-29 12:14:57 +0100

    pep8 on exceptions + added NotFoundError

--- SolrClient/exceptions.py
+++ SolrClient/exceptions.py
@@ -1,20 +1,28 @@
-
 class SolrError(Exception):
     """
     Class to handle any issues that Solr Reports
     """
-    
+
+
 class SolrResponseError(Exception):
-    '''
+    """
     Errors relatd to parsing Solr Response
-    '''
-    
+    """
+
+
 class ConnectionError(Exception):
-    '''
+    """
     Errors connecting to Solr
-    '''
-    
+    """
+
+
 class ZookeeperError(Exception):
-    '''
+    """
     Errors connecting to Zookeeper
-    '''
+    """
+
+
+class NotFoundError(Exception):
+    """
+    When a document wasn't found
+    """
