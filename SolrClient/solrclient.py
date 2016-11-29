36feaab67c96423a2a3670585ba5a8f2188e5790
Dorian <dorian.hoxha@gmail.com>
2016-11-29 13:25:28 +0100

    added .get() to retrieve document + test

--- SolrClient/solrclient.py
+++ SolrClient/solrclient.py
@@ -136,6 +136,26 @@ class SolrClient:
         else:
             return False
 
+    def get(self, collection, doc_id, **kwargs):
+        """
+        :param str collection: The name of the collection for the request
+        :param str doc_id: ID of the document to be retrieved.
+
+        Retrieve document from Solr based on the ID. ::
+
+            >>> solr.get('SolrClient_unittest','changeme')
+        """
+
+        resp, con_inf = self.transport.send_request(method='GET',
+                                                    endpoint='get',
+                                                    collection=collection,
+                                                    params={'id': doc_id},
+                                                    **kwargs)
+        doc = resp['doc']
+        if not doc:
+            raise NotFoundError
+        return doc
+
     def delete_doc_by_id(self, collection, doc_id, **kwargs):
         """
         :param str collection: The name of the collection for the request
