149b317ffc900a54066037c508587b0aee1cc283
Dorian <dorian.hoxha@gmail.com>
2016-12-04 10:55:22 +0100

    added 1-time ShuffleRouter

--- SolrClient/routers/plain.py
+++ SolrClient/routers/plain.py
@@ -6,6 +6,14 @@ class PlainRouter(BaseRouter):
         return self.hosts
 
 
+class ShuffleRouter(BaseRouter):
+    def __init__(self, solr, hosts):
+        super().__init__(solr, hosts)
+        # only shuffle the hosts once, so they're in a different order in all your processes
+        # not all proesses contact the same host, but distribute requests
+        self.shuffle_hosts()
+
+
 class RandomRouter(BaseRouter):
     def get_hosts(self, **kwargs):
         self.shuffle_hosts()
