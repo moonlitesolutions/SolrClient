afd4c1680817937ecdfa7f1825b7ba7595447d29
Dorian <dorian.hoxha@gmail.com>
2016-12-31 20:59:32 +0100

    added test to check exception is correctly raised if solr is down

--- test/test_client.py
+++ test/test_client.py
@@ -45,6 +45,15 @@ class ClientTestIndexing(unittest.TestCase):
         # softCommit because we don't care about data on disk
         self.solr.commit(test_config['SOLR_COLLECTION'], openSearcher=True, softCommit=True)
 
+    def test_down_solr_exception(self):
+        # connect to "down" sorl host
+        s = SolrClient('http://localhost:8999/solr', devel=True)
+        try:
+            s.query('test', {}) # making dumb query
+            raise Exception("If solr is down it should error. Either solr isn't down or code is broken.")
+        except ConnectionError:
+            pass
+
     def test_delete_doc_by_id_with_space(self):
         self.delete_docs()
         self.solr.index_json(test_config['SOLR_COLLECTION'], json.dumps(
